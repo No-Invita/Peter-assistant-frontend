254974c4a44bcb6cc97040cd465c454066b0bc10
Elkin <ersantis@uninorte.edu.co>
2022-05-13 22:27:50 -0500

    map working , i think...

--- map/localized.js
+++ map/localized.js
@@ -1,35 +1,31 @@
-var map = L.map("map");
-var http = require('http');
-const {coordenadas} = require('./js/index');
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+var map = L.map('map');
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var control = L.Routing.control(
-  L.extend(window.lrmConfig, {
-    waypoints: [
-      L.latLng(coordenadas.data, coordenadas.longitude), // Here go the current location
-      L.latLng(11.0209, -74.85179), // Here go the destiny location
-    ],
-    router: L.Routing.graphHopper("ef80e7e7-23d7-470c-8cef-c42f10a83d58", {
-      urlParameters: {
-        vehicle: "foot",
-      },
+var control = L.Routing.control(L.extend(window.lrmConfig, {
+	waypoints: [
+		L.latLng(11.01968,-74.84978),// Here go the current location
+		L.latLng(11.0209,-74.85179) // Here go the destiny location
+	],
+    router: L.Routing.graphHopper('ef80e7e7-23d7-470c-8cef-c42f10a83d58' , {
+        urlParameters: {
+            vehicle: 'foot'
+        }
     }),
-    language: "en",
-    geocoder: L.Control.Geocoder.nominatim(),
+    language: 'en',
+	geocoder: L.Control.Geocoder.nominatim(),
     routeWhileDragging: true,
     reverseWaypoints: true,
     showAlternatives: true,
-    eOptions: {
-      styles: [
-        { color: "black", opacity: 0.15, weight: 9 },
-        { color: "white", opacity: 0.8, weight: 6 },
-        { color: "blue", opacity: 0.5, weight: 2 },
-      ],
-    },
-  })
-).addTo(map);
+   eOptions: {
+        styles: [
+            {color: 'black', opacity: 0.15, weight: 9},
+            {color: 'white', opacity: 0.8, weight: 6},
+            {color: 'blue', opacity: 0.5, weight: 2}
+        ]
+    }
+})).addTo(map);
 
 L.Routing.errorControl(control).addTo(map);
