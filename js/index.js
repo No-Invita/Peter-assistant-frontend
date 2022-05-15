74abb49a1314d48adbef9fcc7c5f39c9aa424e6d
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-05-14 22:34:59 -0500

    stuff

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
-document.addEventListener("DOMContentLoaded", funcionInit);
+const $peter = document.getElementById("peter");
 
+peter.addEventListener("click", function () {
+	speak("Hola, ¿Qué tal?, ¿Quieres saber cual es tu proxima clase?");
+	recognition.start();
+	if (texto.includes("quiero")) {
+		console.log("estas son tus clases");
+	} else {
+		console.log("first");
+	}
+});
+
+document.addEventListener("DOMContentLoaded", funcionInit);
 const displayframe = () => {
 	document.getElementById("frame").style.display = "inline";
-	console.log("first");
 };
 
 document.getElementById("link").addEventListener("click", displayframe);
