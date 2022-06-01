23436285689f05fb4a4ebbd77765fe1a78760b7a
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-05-31 19:09:30 -0500

    unnecesary files deleted

--- js/getclasses.js
+++
@@ -1,9 +0,0 @@
-async function getclasses() {
-	handleAuthClick();
-	const req = await fetch("https://peter-assistant.herokuapp.com/");
-	const res = await req.json();
-	console.log(res);
-	renderClasses(res);
-}
-
-// getclasses();
